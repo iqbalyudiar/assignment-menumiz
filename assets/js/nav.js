b7d4dcb4f8357db39997c894d3299a46304eb251
Iqbal Yudiar <iqbalyudiar123@gmail.com>
2024-09-03 10:06:19 +0700

    feat: add state dashboard active

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,35 +1,90 @@
 const sidebarItems = [
-    { title: 'Dashboard', image: './assets/img/Dashboard-Icon.svg', link: 'index.html' },
-    { title: 'Calendar', image: './assets/img/Calendar-Icon.svg', link: '#' },
-    { title: 'Recruitment', image: './assets/img/Recruitment-Icon.svg', link: '#' },
-    { title: 'Employees', image: './assets/img/Employees-Icon.svg', link: '#' },
-    { title: 'Leave', image: './assets/img/Leave-Icon.svg', link: '#' },
-    { title: 'Organisation', image: './assets/img/Organisation-Icon.svg', link: '#' },
-    { title: 'Billings', image: './assets/img/Billings-Icon.svg', link: '#' },
-    { title: 'Engagement', image: './assets/img/Engagement-Icon.svg', link: '#' },
-    { title: 'Development', image: './assets/img/Development-Icon.svg', link: '#' },
-    { title: 'Performance', image: './assets/img/Performance-Icon.svg', link: '#' },
-    { title: 'Reports', image: './assets/img/Reports-Icon.svg', link: '#' },
+  {
+    title: "Dashboard",
+    image: "./assets/img/Dashboard-Icon.svg",
+    link: "index.html",
+    active: false,
+  },
+  {
+    title: "Calendar",
+    image: "./assets/img/Calendar-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Recruitment",
+    image: "./assets/img/Recruitment-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Employees",
+    image: "./assets/img/Employees-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Leave",
+    image: "./assets/img/Leave-Icon.svg",
+    link: "index.html",
+    active: true,
+  },
+  {
+    title: "Organisation",
+    image: "./assets/img/Organisation-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Billings",
+    image: "./assets/img/Billings-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Engagement",
+    image: "./assets/img/Engagement-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Development",
+    image: "./assets/img/Development-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Performance",
+    image: "./assets/img/Performance-Icon.svg",
+    link: "#",
+    active: false,
+  },
+  {
+    title: "Reports",
+    image: "./assets/img/Reports-Icon.svg",
+    link: "#",
+    active: false,
+  },
 ];
 
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.getElementById("sidebar");
 
-sidebarItems.forEach(item => {
-    const itemLink = document.createElement('a');
-    itemLink.className = 'list-sidebar-item'; 
-    itemLink.href = item.link; 
+sidebarItems.forEach((item) => {
+  const itemLink = document.createElement("a");
+  itemLink.className = `list-sidebar-item ${item.active ? 'item-active' : ''}`;
+  itemLink.href = item.link;
 
-    const img = document.createElement('img');
-    img.src = item.image;
-    img.className = 'list-sidebar-icon mr-16'; 
-    img.alt = `${item.title} Icon`;
+  const img = document.createElement("img");
+  img.src = item.image;
+  img.className = "list-sidebar-icon mr-16";
+  img.alt = `${item.title} Icon`;
 
-    const title = document.createElement('span');
-    title.className = 'list-sidebar-title';
-    title.textContent = item.title;
+  const title = document.createElement("span");
+  title.className = "list-sidebar-title";
+  title.textContent = item.title;
 
-    itemLink.appendChild(img);
-    itemLink.appendChild(title);
+  itemLink.appendChild(img);
+  itemLink.appendChild(title);
 
-    sidebar.appendChild(itemLink);
+  sidebar.appendChild(itemLink);
 });
